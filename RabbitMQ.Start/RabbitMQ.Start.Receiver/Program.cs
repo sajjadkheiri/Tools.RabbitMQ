7adc0a998783c5b9779e44c484bfd4c88712cc85
sajjad <sajjadkheiri7@gmail.com>
2024-12-26 23:33:16 +0330

    Starter project is ended

--- RabbitMQ.Start/RabbitMQ.Start.Receiver/Program.cs
+++ RabbitMQ.Start/RabbitMQ.Start.Receiver/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
-Console.WriteLine("Hello, World!");
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+
+var connection = await factory.CreateConnectionAsync();
+var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync("hello", false, false, false);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+consumer.ReceivedAsync += ConsumerOnReceivedAsync;
+
+await channel.BasicConsumeAsync("hello", false, consumer);
+
+Console.WriteLine("Press [Enter] to exit...");
+Console.ReadLine();
+
+Task ConsumerOnReceivedAsync(object sender, BasicDeliverEventArgs @event)
+{
+    var body = @event.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    
+    Console.WriteLine($" [+] Message is \"{message}\"");
+    
+    return Task.CompletedTask;
+}
--- RabbitMQ.Start/RabbitMQ.Start.Sender/Program.cs
+++ RabbitMQ.Start/RabbitMQ.Start.Sender/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using RabbitMQ.Client;
 
-Console.WriteLine("Hello, World!");
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+
+var connection = await factory.CreateConnectionAsync();
+var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync("hello", false, false, false);
+
+Console.WriteLine(" [*] Type your messages : ");
+
+var message = Console.ReadLine() ?? "Default message";
+var body = Encoding.UTF8.GetBytes(message).ToArray();
+
+await channel.BasicPublishAsync("", "hello", body);
+
+Console.WriteLine(" [x] Sent {0}", message);
+
+await channel.CloseAsync();
